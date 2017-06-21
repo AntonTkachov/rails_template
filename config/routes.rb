f35fe9ab20424c3fb7855ed80eeb6d72f2a2b589
AlexMiasoiedov <alexerna8@gmail.com>
2017-06-21 21:09:11 +0300

    setup device

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root to: 'users#index'
 
-  devise_for :users
-
   resources :members
+  devise_for :users, controllers: { registrations: 'users/registrations',
+                                    sessions: 'users/sessions'
+                                  }
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users
 end
